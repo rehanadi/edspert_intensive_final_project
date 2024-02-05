d310a7905256d24a37ed419abd73b3964134dbe1
Rehan <rehanadipurwana@gmail.com>
2024-02-05 18:15:59 +0700

    Initial commit

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('NONTON·ID')
+        ],
+      ),
+    );
+  }
+}
